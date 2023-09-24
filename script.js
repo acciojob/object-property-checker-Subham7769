9dc587b8251148955f7e26e90b210d0147cdd1b2
Subham jain <134865513+Subham7769@users.noreply.github.com>
2023-09-24 15:29:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,17 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	return sampleObject.includes(key);
+for (let key1 in sampleObject) {
+    if (sampleObject.hasOwnProperty(key1) && key1 === key) {
+      return true;
+    }
+  }
+  return false;
+}
+
+	
 }
 
 // Do not change the code below
-// const key = prompt("Enter Key.");
-// alert(hasKey(key));
+const key = prompt("Enter Key.");
+alert(hasKey(key));
